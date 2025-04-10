424227e25fca1541d58ef47905a3e2bd224cacb9
kaofreitas <142507653+kaofreitas@users.noreply.github.com>
2025-04-10 20:21:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -21,5 +21,27 @@ document.addEventListener('DOMContentLoaded',function(){
     const alternaContraste = document.getElementById('alterna-contraste');
     
 
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+
     })
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+
+    })
+
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+    })
+
+})
+ScrollReveal().reveal('#inicio',{ delay: 500 });
+ScrollReveal().reveal('#tropicalia',{ delay: 500 });
+ScrollReveal().reveal('#galeria',{ delay: 500 });
+ScrollReveal().reveal('#contato',{ delay: 500 });
 
